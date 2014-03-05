e7e65523ce52e38f90ed6ae65b14c6f6ea7ad14e
Benjamin Henne <henne@dcsec.uni-hannover.de>
2014-03-05 21:19:51 +0100

    fixed build with pepper_33 and latest naclports (Feb 2014) - tested with Chrome extension using exiv2

--- ports/exiv2/build.sh
+++ ports/exiv2/build.sh
@@ -5,76 +5,8 @@
 # 
 # The original file was modified by Maximilian Koch and Benjamin Henne
 
-CustomConfigureStep() {
-  local EXTRA_CONFIGURE_OPTS=("${@:-}")
-  Banner "Configuring ${PACKAGE_NAME}"
-  # export the nacl tools
-  export CC=${NACLCC}
-  export CXX=${NACLCXX}" "-lpthread
-  export AR=${NACLAR}
-  export RANLIB=${NACLRANLIB}
-  export PKG_CONFIG_PATH=${NACLPORTS_LIBDIR}/pkgconfig
-  export PKG_CONFIG_LIBDIR=${NACLPORTS_LIBDIR}
-  export FREETYPE_CONFIG=${NACLPORTS_LIBDIR}/freetype-config
-  export PATH=${NACL_BIN_PATH}:${PATH};
-  ChangeDir ${NACL_PACKAGES_REPOSITORY}/${PACKAGE_NAME}
-  #Remove ${PACKAGE_NAME}-build
-  #MakeDir ${PACKAGE_NAME}-build
-  #cd ${PACKAGE_NAME}-build
-  echo "Directory: $(pwd)"
+# exiv2 doesn't support custom build directories so we have
+# to build directly in the source dir. It dies not work due
+# to integrated xmpsdk.
+BUILD_DIR=${SRC_DIR}
 
-  local conf_host=${NACL_CROSS_PREFIX}
-  if [[ ${NACL_ARCH} == "pnacl" ]]; then
-    # The PNaCl tools use "pnacl-" as the prefix, but config.sub
-    # does not know about "pnacl".  It only knows about "le32-nacl".
-    # Unfortunately, most of the config.subs here are so old that
-    # it doesn't know about that "le32" either.  So we just say "nacl".
-    conf_host="nacl"
-  fi
-  ./configure \
-    --host=${conf_host} \
-    --disable-shared \
-    --prefix=${NACLPORTS_PREFIX} \
-    --exec-prefix=${NACLPORTS_PREFIX} \
-    --libdir=${NACLPORTS_LIBDIR} \
-    --oldincludedir=${NACLPORTS_INCLUDE} \
-    --${NACL_OPTION}-mmx \
-    --${NACL_OPTION}-sse \
-    --${NACL_OPTION}-sse2 \
-    --${NACL_OPTION}-asm \
-    "${EXTRA_CONFIGURE_OPTS[@]}"
-}
-
-CustomBuildStep() {
- BUILD_DIR=${SRC_DIR}
- # 
- ChangeDir ${BUILD_DIR}
- # Build ${MAKE_TARGETS} or default target if it is not defined
- if [ -n "${MAKEFLAGS:-}" ]; then
-   echo "MAKEFLAGS=${MAKEFLAGS}"
-   export MAKEFLAGS
- fi
- if [ "${VERBOSE:-}" = "1" ]; then
-   MAKE_TARGETS+=" VERBOSE=1 V=1"
- fi
- export PATH=${NACL_BIN_PATH}:${PATH}
- LogExecute make -j${OS_JOBS} ${MAKE_TARGETS:-}
-}
-
-CustomPackageInstall() {
-  DefaultPreInstallStep
-  DefaultDownloadStep
-  DefaultExtractStep
-  DefaultPatchStep
-  CustomConfigureStep
-  #DefaultConfigureStep
-  #DefaultBuildStep
-  CustomBuildStep
-  DefaultInstallStep
-  #DefaultCleanUpStep
-}
-
-
-CustomPackageInstall
-#DefaultPackageInstall
-exit 0
